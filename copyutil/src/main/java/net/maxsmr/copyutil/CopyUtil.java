e37f53c7cc743a17a1a78f6c958f4aed11db0c9c
msmirnov <maksim.v.smirnov@altarix.ru>
2018-09-14 23:04:04 +0400

    version 1.0.2.1
    ArgsParser refactoring; added searching unknown arguments, exec time

--- copyutil/src/main/java/net/maxsmr/copyutil/CopyUtil.java
+++ copyutil/src/main/java/net/maxsmr/copyutil/CopyUtil.java
@@ -1,5 +1,6 @@
 package net.maxsmr.copyutil;
 
+import net.maxsmr.copyutil.utils.ArgsParser;
 import net.maxsmr.copyutil.utils.CompareUtils;
 import net.maxsmr.copyutil.utils.FileHelper;
 import net.maxsmr.copyutil.utils.Predicate;
@@ -16,20 +17,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static net.maxsmr.copyutil.utils.ArgsParser.findArg;
-import static net.maxsmr.copyutil.utils.ArgsParser.getPairArg;
-
 public class CopyUtil {
 
     // new File("") == jar working dir
 
     private static final BaseLogger logger;
 
+    private static final String lineSeparator = "line.separator";
+
     private static final String[] argsNames =
             {"-pathsListFile", "-sourcePath", "-destinationPath", "-renameFiles", "-deleteEmptyDirs", "-deleteCopiedFiles", "-ignoreExcludedPaths", "-excludeSourcePathsFile"};
 
     private static final String[] excludedPaths =
             {"Boot", "Documents and Settings", "ProgramData", "Program Files", "Program Files (x86)", "Recovery", "System Volume Information", "Windows", "Users"};
+
 
     static {
         BaseLoggerHolder.initInstance(() -> new BaseLoggerHolder(false) {
@@ -45,6 +46,8 @@ public class CopyUtil {
         logger = BaseLoggerHolder.getInstance().getLogger(CopyUtil.class);
     }
 
+    private static ArgsParser argsParser;
+
     private static File pathsToHandleListFile;
     private static List<String> pathsToHandleList;
     private static File excludeSourcePathsListFile;
@@ -58,39 +61,39 @@ public class CopyUtil {
     private static boolean allowDeleteCopiedFiles;
     private static boolean allowIgnoreExcludedPaths;
 
-    public static String getPathsListFile(String args[]) {
-        return getPairArg(args, findArg(argsNames, args, 0, true));
+    private static String getPathsListFile() {
+        return argsParser.getPairArg(argsParser.findArgWithIndex(0, true));
     }
 
-    public static String getSourcePath(String args[]) {
-        return getPairArg(args, findArg(argsNames, args, 1, true));
+    private static String getExcludeSourcePathsListFile() {
+        return argsParser.getPairArg(argsParser.findArgWithIndex(7, true));
     }
 
-    public static String getDestinationPath(String args[]) {
-        return getPairArg(args, findArg(argsNames, args, 2, true));
+    private static String getSourcePath() {
+        return argsParser.getPairArg(argsParser.findArgWithIndex(1, true));
     }
 
-    public static boolean allowRenameFiles(String args[]) {
-        return findArg(argsNames, args, 3, true) != null;
+    private static String getDestinationPath() {
+        return argsParser.getPairArg(argsParser.findArgWithIndex(2, true));
     }
 
-    public static boolean allowDeleteEmptyDirs(String args[]) {
-        return findArg(argsNames, args, 4, true) != null;
+    private static boolean allowRenameFiles() {
+        return argsParser.findArgWithIndex(3, true) != null;
     }
 
-    public static boolean allowDeleteCopiedFiles(String args[]) {
-        return findArg(argsNames, args, 5, true) != null;
+    private static boolean allowDeleteEmptyDirs() {
+        return argsParser.findArgWithIndex(4, true) != null;
     }
 
-    public static boolean allowIgnoreExcludedPaths(String args[]) {
-        return findArg(argsNames, args, 6, true) != null;
+    private static boolean allowDeleteCopiedFiles() {
+        return argsParser.findArgWithIndex(5, true) != null;
     }
 
-    public static String getExcludeSourcePathsListFile(String args[]) {
-        return getPairArg(args, findArg(argsNames, args, 7, true));
+    private static boolean allowIgnoreExcludedPaths() {
+        return argsParser.findArgWithIndex(6, true) != null;
     }
 
-    public static boolean isFileAllowed(File file, boolean isSource) {
+    private static boolean isFileAllowed(File file, boolean isSource) {
         if (file == null) {
             return false;
         }
@@ -102,7 +105,6 @@ public class CopyUtil {
             }
         }
         if (isSource) {
-
             if (sourcePathFile == null) {
                 throw new RuntimeException("sourcePathFile is not initialized");
             }
@@ -114,19 +116,19 @@ public class CopyUtil {
             final String part = TextUtils.trim(filePath.substring(prefix.length(), filePath.length()), CompareUtils.Condition.EQUAL, File.separatorChar, true, true);
             String restrictedRelativePath = Predicate.Methods.find(excludeSourcePathsList, element -> !TextUtils.isEmpty(element) && !element.equals(File.separator) && part.startsWith(element));
             if (!TextUtils.isEmpty(restrictedRelativePath)) {
-                logger.e("Source file \"" + file + "\" is excluded by relative path \"" + restrictedRelativePath + "\" from list");
+                logger.e("Source file \"" + file + "\" is excluded by relative path \"" + restrictedRelativePath + "\" from list in \"" + excludeSourcePathsListFile.getName() + "\"");
                 return false;
             }
         }
         return true;
     }
 
-    public static boolean isDestinationDirAllowed(File sourceFile, File destinationDir) {
+    private static boolean isDestinationDirAllowed(File sourceFile, File destinationDir) {
         if (destinationDir == null) {
             return false;
         }
         if (destinationDir.getParentFile() == null) {
-            logger.e("Root of partition is not allowed (destination directory \"" + destinationDir + (sourceFile != null? "\" and source file \"" + sourceFile + "\")" : "\""));
+            logger.e("Root of partition is not allowed (destination directory \"" + destinationDir + (sourceFile != null ? "\" and source file \"" + sourceFile + "\")" : "\""));
             return false;
         }
         return true;
@@ -134,13 +136,16 @@ public class CopyUtil {
 
     public static void main(String args[]) {
 
-        logger.i(System.getProperty("line.separator"));
+        logger.i(System.getProperty(lineSeparator) + CopyUtil.class.getSimpleName() + ", version: 1.0.2.1"); // FIXME
 
         if (args == null || args.length == 0) {
             throw new IllegalArgumentException("Args not specified!");
         }
 
-        final String pathsToHandleListStr = TextUtils.trim(getPathsListFile(args), false, true);
+        argsParser = new ArgsParser(argsNames);
+        argsParser.setArgs(args);
+
+        final String pathsToHandleListStr = TextUtils.trim(getPathsListFile(), false, true);
         if (!TextUtils.isEmpty(pathsToHandleListStr)) {
             FileHelper.checkFile(pathsToHandleListStr, false);
             pathsToHandleListFile = new File(pathsToHandleListStr);
@@ -148,7 +153,7 @@ public class CopyUtil {
             pathsToHandleListFile = null;
         }
 
-        final String excludeSourcePathsListStr = TextUtils.trim(getExcludeSourcePathsListFile(args), false, true);
+        final String excludeSourcePathsListStr = TextUtils.trim(getExcludeSourcePathsListFile(), false, true);
         if (!TextUtils.isEmpty(excludeSourcePathsListStr)) {
             FileHelper.checkFile(excludeSourcePathsListStr, false);
             excludeSourcePathsListFile = new File(excludeSourcePathsListStr);
@@ -156,12 +161,12 @@ public class CopyUtil {
             excludeSourcePathsListFile = null;
         }
 
-        final String sourcePath = TextUtils.trim(getSourcePath(args), false, true);
+        final String sourcePath = TextUtils.trim(getSourcePath(), false, true);
         FileHelper.checkDir(sourcePath, false);
         sourcePathFile = new File(sourcePath);
         logger.i("Source path to copy/move from: \"" + sourcePathFile + "\"");
 
-        final String destinationPath = TextUtils.trim(getDestinationPath(args), false, true);
+        final String destinationPath = TextUtils.trim(getDestinationPath(), false, true);
         if (TextUtils.isEmpty(destinationPath)) {
             throw new IllegalArgumentException("Destination path is not specified");
         }
@@ -173,10 +178,17 @@ public class CopyUtil {
             throw new IllegalArgumentException("Source path \"" + sourcePathFile + "\" is same as destination path \"" + destinationPathFile + "\"");
         }
 
-        allowRenameFiles = allowRenameFiles(args);
-        allowDeleteEmptyDirs = allowDeleteEmptyDirs(args);
-        allowDeleteCopiedFiles = allowDeleteCopiedFiles(args);
-        allowIgnoreExcludedPaths = allowIgnoreExcludedPaths(args);
+        allowRenameFiles = allowRenameFiles();
+        allowDeleteEmptyDirs = allowDeleteEmptyDirs();
+        allowDeleteCopiedFiles = allowDeleteCopiedFiles();
+        allowIgnoreExcludedPaths = allowIgnoreExcludedPaths();
+
+        Set<Integer> unhandledIndexes = argsParser.getUnhandledArgsIndexes();
+        for (Integer index : unhandledIndexes) {
+            if (index != null) {
+                logger.e("Unknown argument \"" + args[index] + "\" (position: " + index + ")");
+            }
+        }
 
         final Map<File, Boolean> resultMap = new LinkedHashMap<>();
 
@@ -188,7 +200,7 @@ public class CopyUtil {
         if (pathsToHandleList.isEmpty()) {
             pathsToHandleList.add(File.separator);
         }
-        logger.i("Relative paths to copy/move:" + System.getProperty("line.separator") + pathsToHandleList + System.getProperty("line.separator"));
+        logger.i("Relative paths to copy/move:" + System.getProperty(lineSeparator) + pathsToHandleList + System.getProperty(lineSeparator));
 
         if (excludeSourcePathsListFile != null) {
             excludeSourcePathsList = FileHelper.readStringsFromFile(excludeSourcePathsListFile);
@@ -196,10 +208,14 @@ public class CopyUtil {
             excludeSourcePathsList = new ArrayList<>();
         }
         if (!excludeSourcePathsList.isEmpty()) {
-            logger.i("Relative paths to exclude from copy/move: " + System.getProperty("line.separator") + excludeSourcePathsList + System.getProperty("line.separator"));
+            logger.i("Relative paths to exclude from copy/move: " + System.getProperty(lineSeparator) + excludeSourcePathsList + System.getProperty(lineSeparator));
         }
 
+        long startTime = System.currentTimeMillis();
+
         for (String relativePath : pathsToHandleList) {
+
+            relativePath = relativePath != null ? TextUtils.trim(relativePath, false, true) : null;
 
             if (TextUtils.isEmpty(relativePath)) {
                 continue;
@@ -290,7 +306,7 @@ public class CopyUtil {
                                     part = part.substring(sourcePathFile.getAbsolutePath().length(), part.length()); // remove source path prefix
                                 }
 
-                                File targetDir = !TextUtils.isEmpty(part)? new File(destinationPathFile, part) : destinationPathFile;
+                                File targetDir = !TextUtils.isEmpty(part) ? new File(destinationPathFile, part) : destinationPathFile;
                                 File targetFile = new File(targetDir, f.getName());
 
                                 if (!isFileAllowed(targetFile, false)
@@ -382,6 +398,8 @@ public class CopyUtil {
             }
         }
 
+        final long execTime = System.currentTimeMillis() - startTime;
+
         final List<Map.Entry<File, Boolean>> succeededFiles = Predicate.Methods.filter(resultMap.entrySet(), Map.Entry::getValue);
         final List<Map.Entry<File, Boolean>> failedFiles = Predicate.Methods.filter(resultMap.entrySet(), element -> !element.getValue());
         logger.i("");
@@ -394,6 +412,6 @@ public class CopyUtil {
             logger.i(FileHelper.filesToString(Predicate.Methods.entriesToKeys(failedFiles), 0));
         }
         logger.i("=======================================");
-        logger.i("Copy/move done; succeeded: " + succeededFiles.size() + ", failed: " + failedFiles.size());
+        logger.i("Copy/move done; succeeded: " + succeededFiles.size() + ", failed: " + failedFiles.size() + ", elapsed time: " + execTime + " ms");
     }
 }
---
+++ copyutil/src/main/java/net/maxsmr/copyutil/utils/JvmUtils.java
@@ -0,0 +1,58 @@
+package net.maxsmr.copyutil.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+public class JvmUtils {
+
+    public static Pair<String, String> getJvmVersion() {
+        String classPath = String.class.getResource(String.class.getSimpleName() + ".class").toString();
+        int index = classPath.lastIndexOf("!");
+        if (index >= 0) {
+            String libPath = classPath.substring(0, index);
+            String filePath = libPath + "!/META-INF/MANIFEST.MF";
+            Manifest manifest = null;
+            try {
+                manifest = new Manifest(new URL(filePath).openStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (manifest != null) {
+                Attributes attrs = manifest.getMainAttributes();
+                return new Pair<>(attrs.getValue("Manifest-Version"), attrs.getValue("Implementation-Version"));
+            }
+        }
+        return null;
+    }
+
+    public static Manifest getJarManifest(Class<?> clazz) {
+        try {
+            Enumeration<URL> resources = clazz.getClassLoader()
+                    .getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                InputStream is = resources.nextElement().openStream();
+                if (is != null) {
+                    return new Manifest(is);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getJarManifestVersion(Class<?> clazz) {
+        Manifest manifest = getJarManifest(clazz);
+        if (manifest != null) {
+            Attributes attrs = manifest.getMainAttributes();
+            if (attrs != null) {
+                return attrs.getValue("Manifest-Version");
+            }
+        }
+        return null;
+    }
+}
